23aafbdba49faeeed9cfdb9c379de8640614bf5a
jonatan1609 <5377074j@gmail.com>
2020-10-29 15:10:20 +0200

    fix small bug

--- plugins/start_handler.py
+++ plugins/start_handler.py
@@ -2,6 +2,6 @@ from pyrogram import Client, types
 from .utils import Filters, Messages
 
 
-@Client.on_message(Filters.START.value)
+@Client.on_message(Filters.START.value, group=-1)
 async def start_handler(_client: Client, message: types.Message):
-    await message.reply(Messages.START.value)
+    await message.reply(Messages.START.value.format(name=message.from_user.first_name))
